0e0b52a1ec87c736c64da2a71bf192c279e40810
Dmitriy <dmitriyvolkov@gmail.com>
2017-06-28 18:47:25 +0300

    Added several pre and post deploy tasks

--- lib/capistrano/helpers/aws_helper.rb
+++ lib/capistrano/helpers/aws_helper.rb
@@ -3,6 +3,78 @@ require 'aws-sdk'
 module AwsHelper
   IP_TYPES = %w(public_ip_address public_dns_name private_ip_address private_dns_name)
 
+  def create_ami_image(aws_region, aws_access_key_id, aws_secret_access_key, aws_autoscaling_group_name, aws_ip_type, aws_instance_type, aws_security_groups)
+    puts 'Create AMI image'
+    name = "#{aws_autoscaling_group_name}-AMI-#{Time.now.to_i}"
+    aws_credentials = Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+    instance = fetch_autoscaling_group_instances(aws_region,  aws_autoscaling_group_name, aws_credentials).first
+    ec2 = Aws::EC2::Client.new(region: aws_region, credentials: aws_credentials)
+
+    old_images = ec2.describe_images(filters: [
+        {
+            name: 'tag:autoscaling_group_name',
+            values: [aws_autoscaling_group_name],
+        },
+        {
+            name: 'tag:application',
+            values: ['rails'],
+        }
+    ])
+
+    image = ec2.create_image({
+                         description: name,
+                         dry_run: false,
+                         instance_id: instance.instance_id, # required
+                         name: name, # required
+                         no_reboot: true
+                     })
+    ec2.create_tags({resources: [image.image_id],
+                     tags: [
+                             {key: 'application', value: 'rails'},
+                             {key: 'autoscaling_group_name', value: aws_autoscaling_group_name},
+                           ]
+                    })
+
+    create_launch_configuration(aws_instance_type, aws_security_groups, image.image_id, aws_region, aws_autoscaling_group_name, aws_credentials )
+
+    old_images.each do |image|
+
+    end
+  end
+
+  def create_launch_configuration(aws_instance_type, aws_security_groups, image_id, aws_region, autoscaling_group_name, aws_credentials)
+    name = "#{autoscaling_group_name}-LC-#{Time.now.to_i}"
+    as = Aws::AutoScaling::Client.new(region: aws_region, credentials: aws_credentials)
+    old_launch_configurations = as.describe_launch_configurations[0].select do |launch_configuration|
+      launch_configuration.launch_configuration_name.include?("#{autoscaling_group_name}-LC")
+    end
+    as.create_launch_configuration({
+
+                                      image_id: image_id,
+                                      instance_type: aws_instance_type,
+                                      launch_configuration_name: name,
+                                      security_groups: aws_security_groups,
+                                  })
+    as.update_auto_scaling_group({
+                                   auto_scaling_group_name: autoscaling_group_name,
+                                   launch_configuration_name: name,
+                               })
+    old_launch_configurations.each do |launch_configuration|
+      as.delete_launch_configuration({launch_configuration_name: launch_configuration.launch_configuration_name})
+    end
+  end
+
+  def update_auto_scale_group(aws_region, aws_access_key_id, aws_secret_access_key, aws_autoscaling_group_name, aws_ip_type, max=nil)
+    aws_credentials = Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+    count = max || get_instances(aws_region, aws_access_key_id, aws_secret_access_key, aws_autoscaling_group_name, aws_ip_type).count
+    as = Aws::AutoScaling::Client.new(region: aws_region, credentials: aws_credentials)
+
+    as.update_auto_scaling_group({
+                                         auto_scaling_group_name: aws_autoscaling_group_name,
+                                         max_size: p(count)
+                                     })
+  end
+
   def get_instances(aws_region, aws_access_key_id, aws_secret_access_key, aws_autoscaling_group_name, aws_ip_type)
     aws_credentials = Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
     retrieve_ec2_instances(aws_region, aws_autoscaling_group_name, aws_credentials, aws_ip_type)
--- lib/capistrano/tasks/autoscaling.rake
+++ lib/capistrano/tasks/autoscaling.rake
@@ -1,6 +1,19 @@
 require 'capistrano/helpers/aws_helper'
 require 'capistrano/helpers/cap_helper'
 
+def autoscale(*args)
+  include Capistrano::DSL
+  include AwsHelper
+  include CapHelper
+  invoke 'autoscaling_deploy:freeze_auto_scaling_group'
+  ec2_instances = fetch_ec2_instances
+  ec2_instances.each do |hostname|
+    server(hostname, *args)
+  end
+
+
+end
+
 namespace :load do
   task :defaults do
     set :aws_autoscaling, true
@@ -11,8 +24,8 @@ namespace :load do
 end
 
 namespace :deploy do
-  before :starting, :check_autoscaling_hooks do
-    invoke 'autoscaling_deploy:setup_instances' if fetch(:aws_autoscaling)
+  after 'deploy:finished', :check_autoscaling_hooks do
+    invoke 'autoscaling_deploy:unfreeze_auto_scaling_group' if fetch(:aws_autoscaling_max_instances)
   end
 end
 
@@ -20,18 +33,54 @@ namespace :autoscaling_deploy do
   include AwsHelper
   include CapHelper
 
+  desc 'Freeze Auto Scaling Group.'
+  task :freeze_auto_scaling_group do
+    region = fetch(:aws_region)
+    key = fetch(:aws_access_key_id)
+    secret = fetch(:aws_secret_access_key)
+    group_name = fetch(:aws_autoscaling_group_name)
+    ip_type = fetch(:aws_ip_type)
+    puts 'Freeze Auto Scaling Group.'
+    update_auto_scale_group(region, key, secret, group_name, ip_type)
+  end
+
+  desc 'Unfreeze Auto Scaling Group.'
+  task :unfreeze_auto_scaling_group do
+    region = fetch(:aws_region)
+    key = fetch(:aws_access_key_id)
+    secret = fetch(:aws_secret_access_key)
+    group_name = fetch(:aws_autoscaling_group_name)
+    ip_type = fetch(:aws_ip_type)
+    autoscaling_max_instances = fetch(:aws_autoscaling_max_instances)
+    instance_type = fetch(:aws_instance_type)
+    security_groups = fetch(:aws_security_groups)
+    create_ami_image(region, key, secret, group_name, ip_type, instance_type, security_groups)
+    puts 'Unfreeze Auto Scaling Group.'
+    update_auto_scale_group(region, key, secret, group_name, ip_type, autoscaling_max_instances)
+  end
+
   desc 'Add server from Auto Scaling Group.'
   task :setup_instances do
     ec2_instances = fetch_ec2_instances
     aws_deploy_roles = fetch(:aws_deploy_roles)
     aws_deploy_user = fetch(:aws_deploy_user)
-
+    aws_ssh_key = fetch(:aws_ssh_key)
     ec2_instances.each {|instance|
       if ec2_instances.first == instance
-        server instance, user: aws_deploy_user, roles: aws_deploy_roles, primary: true
+        server instance, user: aws_deploy_user, roles: aws_deploy_roles, primary: true,
+               ssh_options: {
+                   keys: [aws_ssh_key],
+                   forward_agent: true,
+                   auth_methods: %w(publickey)
+               }
         puts("First Server: #{instance} - #{aws_deploy_roles}")
       else
-        server instance, user: aws_deploy_user, roles: sanitize_roles(aws_deploy_roles)
+        server instance, user: aws_deploy_user, roles: sanitize_roles(aws_deploy_roles),
+               ssh_options: {
+                   keys: [aws_ssh_key],
+                   forward_agent: true,
+                   auth_methods: %w(publickey)
+               }
         puts("Server: #{instance} - #{sanitize_roles(aws_deploy_roles)}")
       end
     }
@@ -44,11 +93,12 @@ namespace :autoscaling_deploy do
     secret = fetch(:aws_secret_access_key)
     group_name = fetch(:aws_autoscaling_group_name)
     ip_type = fetch(:aws_ip_type)
-
+    #update_auto_scale_group(region, key, secret, group_name, ip_type)
     instances = get_instances(region, key, secret, group_name, ip_type)
 
     puts("Found #{instances.count} servers (#{instances.join(',')}) for Auto Scaling Group: #{group_name} ")
 
     instances
   end
+
 end
